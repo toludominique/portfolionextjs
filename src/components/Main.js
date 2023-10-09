c607b5992291f0a3917c2fb272a9ef47033967ec
tolutope oguntola <tolutopeoguntola@yahoo.com>
2023-10-09 21:35:34 +0100

    error handling

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,11 +8,11 @@ function Main() {
     <div id='home' className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div >
-            <p className='uppercase text-sm tracking-widest text-gray-600'>LET'S BUILD SOMETHING TOGETHER</p>
-            <h1 className='py-4 text-gray-700'>Hi I'm <span className='text-[#5651e5]'>Tolu</span></h1>
+            <p className='uppercase text-sm tracking-widest text-gray-600'>LET&apos;S BUILD SOMETHING TOGETHER</p>
+            <h1 className='py-4 text-gray-700'>Hi I&apos;m <span className='text-[#5651e5]'>Tolu</span></h1>
             <h1 className='py-2 text-gray-700'>A FULLSTACK DEVELOPER</h1>
             <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
-            I’m focused on building responsive front-end web applications
+            I&apos;m focused on building responsive front-end web applications
             integrating back-end technologies.
           </p>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
